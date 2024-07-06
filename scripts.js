196b883ef04dc37f24842f9ed48bc9437a3c6ae3
Osvaldo <ocasabella98@gmail.com>
2024-07-05 21:40:03 -0300

    version 1.1

--- scripts.js
+++ scripts.js
@@ -3,60 +3,33 @@ function calcular() {
     var metrosGaleria = parseFloat(document.getElementById("metrosGaleria").value) || 0;
     var metrosPergola = parseFloat(document.getElementById("metrosPergola").value) || 0;
 
-    // Resultados para Obra Gris
-    var resultadoObraGrisStandard = calcularObraGris(metrosCubiertos, metrosGaleria, metrosPergola);
-    var resultadoObraGrisECAPStandard = calcularObraGrisConECAP(metrosCubiertos, metrosGaleria, metrosPergola);
-    
-    // Resultados para Llave en Mano
-    var resultadoLlaveEnManoStandard = calcularLlaveEnMano(metrosCubiertos, metrosGaleria, metrosPergola);
-    var resultadoLlaveEnManoECAPStandard = calcularLlaveEnManoConECAP(metrosCubiertos, metrosGaleria, metrosPergola);
+    var obraGrisStandard = metrosCubiertos * 254100 + metrosPergola * 100000 + metrosGaleria * 184800;
+    var obraGrisPremium = metrosCubiertos * 308000 + metrosPergola * 100000 + metrosGaleria * 184800;
 
-    // Resultados para Premium
-    var resultadoObraGrisPremium = calcularPremiumBase(metrosCubiertos, metrosGaleria, metrosPergola);
-    var resultadoObraGrisECAPPremium = calcularPremiumConECAP(metrosCubiertos, metrosGaleria, metrosPergola);
-    var resultadoLlaveEnManoPremium = calcularPremiumLlaveEnMano(metrosCubiertos, metrosGaleria, metrosPergola);
-    var resultadoLlaveEnManoECAPPremium = calcularPremiumLlaveEnManoConECAP(metrosCubiertos, metrosGaleria, metrosPergola);
+    var obraGrisECAPStandard = obraGrisStandard + metrosCubiertos * 50000;
+    var obraGrisECAPPremium = obraGrisPremium + metrosCubiertos * 50000;
 
-    // Mostrar resultados
-    document.getElementById("resultadoObraGrisStandard").innerText = resultadoObraGrisStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoObraGrisECAPStandard").innerText = resultadoObraGrisECAPStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoLlaveEnManoStandard").innerText = resultadoLlaveEnManoStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoLlaveEnManoECAPStandard").innerText = resultadoLlaveEnManoECAPStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoObraGrisPremium").innerText = resultadoObraGrisPremium.toLocaleString('es-ES');
-    document.getElementById("resultadoObraGrisECAPPremium").innerText = resultadoObraGrisECAPPremium.toLocaleString('es-ES');
-    document.getElementById("resultadoLlaveEnManoPremium").innerText = resultadoLlaveEnManoPremium.toLocaleString('es-ES');
-    document.getElementById("resultadoLlaveEnManoECAPPremium").innerText = resultadoLlaveEnManoECAPPremium.toLocaleString('es-ES');
+    var llaveEnManoStandard = metrosCubiertos * 429000 + metrosPergola * 181500 + metrosGaleria * 300300;
+    var llaveEnManoPremium = metrosCubiertos * 506000 + metrosPergola * 181500 + metrosGaleria * 300300;
+
+    var llaveEnManoECAPStandard = llaveEnManoStandard + metrosCubiertos * 50000;
+    var llaveEnManoECAPPremium = llaveEnManoPremium + metrosCubiertos * 50000;
+
+    document.getElementById("resultadoObraGrisStandard").innerText = obraGrisStandard.toLocaleString('es-ES');
+    document.getElementById("resultadoObraGrisPremium").innerText = obraGrisPremium.toLocaleString('es-ES');
+
+    document.getElementById("resultadoObraGrisECAPStandard").innerText = obraGrisECAPStandard.toLocaleString('es-ES');
+    document.getElementById("resultadoObraGrisECAPPremium").innerText = obraGrisECAPPremium.toLocaleString('es-ES');
+
+    document.getElementById("resultadoLlaveEnManoStandard").innerText = llaveEnManoStandard.toLocaleString('es-ES');
+    document.getElementById("resultadoLlaveEnManoPremium").innerText = llaveEnManoPremium.toLocaleString('es-ES');
+
+    document.getElementById("resultadoLlaveEnManoECAPStandard").innerText = llaveEnManoECAPStandard.toLocaleString('es-ES');
+    document.getElementById("resultadoLlaveEnManoECAPPremium").innerText = llaveEnManoECAPPremium.toLocaleString('es-ES');
 }
 
-// Funciones de cálculo
-function calcularObraGris(cubierto, galeria, pergola) {
-    return cubierto * 254100 + galeria * 184800 + pergola * 100000;
-}
-
-function calcularObraGrisConECAP(cubierto, galeria, pergola) {
-    return calcularObraGris(cubierto, galeria, pergola) + cubierto * 50000;
-}
-
-function calcularLlaveEnMano(cubierto, galeria, pergola) {
-    return cubierto * 429000 + galeria * 300300 + pergola * 181500;
-}
-
-function calcularLlaveEnManoConECAP(cubierto, galeria, pergola) {
-    return calcularLlaveEnMano(cubierto, galeria, pergola) + cubierto * 50000;
-}
-
-function calcularPremiumBase(cubierto, galeria, pergola) {
-    return cubierto * 308000 + galeria * 184800 + pergola * 100000;
-}
-
-function calcularPremiumConECAP(cubierto, galeria, pergola) {
-    return calcularPremiumBase(cubierto, galeria, pergola) + cubierto * 50000;
-}
-
-function calcularPremiumLlaveEnMano(cubierto, galeria, pergola) {
-    return cubierto * 506000 + galeria * 300300 + pergola * 181500;
-}
-
-function calcularPremiumLlaveEnManoConECAP(cubierto, galeria, pergola) {
-    return calcularPremiumLlaveEnMano(cubierto, galeria, pergola) + cubierto * 50000;
+function toggleTheme() {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dark" ? "light" : "dark";
+    document.documentElement.setAttribute("data-theme", newTheme);
 }
