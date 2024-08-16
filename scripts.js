2e2121219f93f2193e39c4d248ec58446105a31a
Osvaldo <ocasabella98@gmail.com>
2024-08-16 14:14:02 -0300

    up five porsant to total value

--- scripts.js
+++ scripts.js
@@ -3,17 +3,17 @@ function calcular() {
     var metrosGaleria = parseFloat(document.getElementById("metrosGaleria").value) || 0;
     var metrosPergola = parseFloat(document.getElementById("metrosPergola").value) || 0;
 
-    var obraGrisStandard = metrosCubiertos * 254100 + metrosPergola * 100000 + metrosGaleria * 184800;
-    var obraGrisPremium = metrosCubiertos * 308000 + metrosPergola * 100000 + metrosGaleria * 184800;
+    var obraGrisStandard = metrosCubiertos * 254100 * 1.05 + metrosPergola * 100000 * 1.05 + metrosGaleria * 184800 * 1.05;
+    var obraGrisPremium = metrosCubiertos * 308000 * 1.05 + metrosPergola * 100000 * 1.05 + metrosGaleria * 184800 * 1.05;
 
-    var obraGrisECAPStandard = obraGrisStandard + metrosCubiertos * 50000;
-    var obraGrisECAPPremium = obraGrisPremium + metrosCubiertos * 50000;
+    var obraGrisECAPStandard = obraGrisStandard + metrosCubiertos * 50000 * 1.05;
+    var obraGrisECAPPremium = obraGrisPremium + metrosCubiertos * 50000 * 1.05;
 
-    var llaveEnManoStandard = metrosCubiertos * 429000 + metrosPergola * 181500 + metrosGaleria * 300300;
-    var llaveEnManoPremium = metrosCubiertos * 506000 + metrosPergola * 181500 + metrosGaleria * 300300;
+    var llaveEnManoStandard = metrosCubiertos * 429000 * 1.05 + metrosPergola * 181500 * 1.05 + metrosGaleria * 300300 * 1.05;
+    var llaveEnManoPremium = metrosCubiertos * 506000 * 1.05 + metrosPergola * 181500 * 1.05 + metrosGaleria * 300300 * 1.05;
 
-    var llaveEnManoECAPStandard = llaveEnManoStandard + metrosCubiertos * 50000;
-    var llaveEnManoECAPPremium = llaveEnManoPremium + metrosCubiertos * 50000;
+    var llaveEnManoECAPStandard = llaveEnManoStandard + metrosCubiertos * 50000 * 1.05;
+    var llaveEnManoECAPPremium = llaveEnManoPremium + metrosCubiertos * 50000 * 1.05;
 
     document.getElementById("resultadoObraGrisStandard").innerText = obraGrisStandard.toLocaleString('es-ES');
     document.getElementById("resultadoObraGrisPremium").innerText = obraGrisPremium.toLocaleString('es-ES');
