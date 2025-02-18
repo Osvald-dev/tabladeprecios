233d29f00cf86f77e7f255a9e5f23056c153e1da
Osvaldo <ocasabella98@gmail.com>
2025-02-18 11:18:48 -0300

    actualización precios 2025

--- scripts.js
+++ scripts.js
@@ -3,41 +3,16 @@ function calcular() {
     var metrosGaleria = parseFloat(document.getElementById("metrosGaleria").value) || 0;
     var metrosPergola = parseFloat(document.getElementById("metrosPergola").value) || 0;
 
-    var obraGrisStandard = metrosCubiertos * 350000 + metrosPergola * 105000 + metrosGaleria * 190000;
-    var obraGrisPremium = metrosCubiertos * 420000 + metrosPergola * 105000 + metrosGaleria * 190000;
-
-    var obraGrisECAPStandard = obraGrisStandard + metrosCubiertos * 70000;
-    var obraGrisECAPPremium = obraGrisPremium + metrosCubiertos * 80000;
-
-    var llaveEnManoStandard = metrosCubiertos * 650000 + metrosPergola * 190000 + metrosGaleria * 310000;
-    var llaveEnManoPremium = metrosCubiertos * 750000 + metrosPergola * 190000 + metrosGaleria * 310000;
-
-    var llaveEnManoECAPStandard = llaveEnManoStandard + metrosCubiertos * 50000;
-    var llaveEnManoECAPPremium = llaveEnManoPremium + metrosCubiertos * 50000;
+    var obraGris = metrosCubiertos * 400000 + metrosPergola * 120000 + metrosGaleria * 250000;
+    var llaveEnMano = metrosCubiertos * 700000 + metrosPergola * 250000 + metrosGaleria * 400000;
 
     var duplex = metrosCubiertos * 420000 + metrosPergola * 105000 + metrosGaleria * 190000;
     var duplexLlaveEnMano = metrosCubiertos * 750000 + metrosPergola * 190000 + metrosGaleria * 310000;
 
-    var duplexECAP = duplex + metrosCubiertos * 80000;
-    var duplexLlaveEnManoECAP = duplexLlaveEnMano + metrosCubiertos * 50000;
-
-    document.getElementById("resultadoObraGrisStandard").innerText = obraGrisStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoObraGrisPremium").innerText = obraGrisPremium.toLocaleString('es-ES');
-
-    document.getElementById("resultadoObraGrisECAPStandard").innerText = obraGrisECAPStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoObraGrisECAPPremium").innerText = obraGrisECAPPremium.toLocaleString('es-ES');
-
-    document.getElementById("resultadoLlaveEnManoStandard").innerText = llaveEnManoStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoLlaveEnManoPremium").innerText = llaveEnManoPremium.toLocaleString('es-ES');
-
-    document.getElementById("resultadoLlaveEnManoECAPStandard").innerText = llaveEnManoECAPStandard.toLocaleString('es-ES');
-    document.getElementById("resultadoLlaveEnManoECAPPremium").innerText = llaveEnManoECAPPremium.toLocaleString('es-ES');
-
+    document.getElementById("resultadoObraGris").innerText = obraGris.toLocaleString('es-ES');
+    document.getElementById("resultadoLlaveEnMano").innerText = llaveEnMano.toLocaleString('es-ES');
     document.getElementById("resultadoDuplex").innerText = duplex.toLocaleString('es-ES');
     document.getElementById("resultadoDuplexLlaveEnMano").innerText = duplexLlaveEnMano.toLocaleString('es-ES');
-
-    document.getElementById("resultadoDuplexECAP").innerText = duplexECAP.toLocaleString('es-ES');
-    document.getElementById("resultadoDuplexLlaveEnManoECAP").innerText = duplexLlaveEnManoECAP.toLocaleString('es-ES');
 }
 
 function toggleTheme() {
