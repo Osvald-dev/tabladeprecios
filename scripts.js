ac1320b5a6db6014b3dd49bd7b2d1d6b995903ca
Osvaldo <ocasabella98@gmail.com>
2025-11-13 13:48:18 -0300

    Actualización de valores metro cubierto

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@ function calcular() {
     var metrosGaleria = parseFloat(document.getElementById("metrosGaleria").value) || 0;
     var metrosPergola = parseFloat(document.getElementById("metrosPergola").value) || 0;
 
-    var obraGris = metrosCubiertos * 450000 + metrosPergola * 120000 + metrosGaleria * 250000;
-    var llaveEnMano = metrosCubiertos * 750000 + metrosPergola * 250000 + metrosGaleria * 400000;
+    var obraGris = metrosCubiertos * 500000 + metrosPergola * 120000 + metrosGaleria * 250000;
+    var llaveEnMano = metrosCubiertos * 800000 + metrosPergola * 250000 + metrosGaleria * 450000;
 
-    var duplex = metrosCubiertos * 470000 + metrosPergola * 120000 + metrosGaleria * 250000;
-    var duplexLlaveEnMano = metrosCubiertos * 800000 + metrosPergola * 250000 + metrosGaleria * 400000;
+    var duplex = metrosCubiertos * 550000 + metrosPergola * 120000 + metrosGaleria * 250000;
+    var duplexLlaveEnMano = metrosCubiertos * 900000 + metrosPergola * 250000 + metrosGaleria * 400000;
 
     document.getElementById("resultadoObraGris").innerText = obraGris.toLocaleString('es-ES');
     document.getElementById("resultadoLlaveEnMano").innerText = llaveEnMano.toLocaleString('es-ES');
